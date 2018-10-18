1057ba0423e9255b540b2602285799eb24059a2d
Acesmile <ab_chen1937@126.com>
2018-10-18 10:38:23 +0800

    测试commoit
    测试commoit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var util = require('./utils/util.js');
 var api = require('./config/api.js');
 var user = require('./services/user.js');
 // 测试抓取
+// 测试提交
 App({
   onLaunch: function () {
     //获取用户的登录信息
